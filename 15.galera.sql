96d28f939155b3408dd25347af651d44948ffb51
Farhang Vafa <farhangv@ymail.com>
2018-08-27 15:28:48 +0430

    1010

--- 15.galera.sql
+++ 15.galera.sql
@@ -1,4 +1,13 @@
 SHOW VARIABLES LIKE '%wsrep%';
+
 SHOW GLOBAL STATUS LIKE 'wsrep_cluster_size';
+
 CREATE SCHEMA `new_db`;
 
+
+
+create user 'myuser'@'%' identified by 'mypwd';
+grant select on mysql.user to 'myuser'@'%';
+grant select on mysql.db to 'myuser'@'%';
+grant select on mysql.tables_priv to 'myuser'@'%';
+grant show databases on *.* to 'myuser'@'%';
